ef9ea55b08bab3ebc84efb3becdc5bfa4271f9fb
weicheng <weicheng.xi@newtouch.cn>
2017-01-18 16:11:12 +0800

    ...

--- src/resources/elements/em-chat-member-popup.js
+++ src/resources/elements/em-chat-member-popup.js
@@ -5,7 +5,6 @@ export class EmChatMemberPopup {
 
     members = [];
     member = {};
-    show = false;
 
     /**
      * 构造函数
@@ -15,37 +14,30 @@ export class EmChatMemberPopup {
             this.channel = payload.channel;
             this.username = payload.username;
             this.target = payload.target;
-            // this.show = false;
+            if (this.username == 'all') {
+                this.members = this.channel.members;
+            } else {
+                this.member = utils.getUser(this.username);
+                let user = utils.getUser(this.member.creator);
+                this.member.creatorName = (user && (!!user.name)) ? user.name : this.member.creator;
+            }
+            _.defer(() => {
 
-            $(this.target).popup({
-                popup: this.popup,
-                hoverable: true,
-                inline: false,
-                silent: true,
-                // movePopup: false,
-                // position: 'top left',
-                position: 'bottom left',
-                maxSearchDepth: 50,
-                delay: {
-                    show: 300,
-                    hide: 300
-                },
-                onVisible: () => {
-                    _.defer(() => {
-                        if (this.username == 'all') {
-                            this.members = this.channel.members;
-                        } else {
-                            this.member = utils.getUser(this.username);
-                            let user = utils.getUser(this.member.creator);
-                            this.member.creatorName = (user && (!!user.name)) ? user.name : this.member.creator;
-                        }
-                        this.show = true;
-                    });
-                },
-                onHidden: () => {
-                    this.show = false;
-                }
-            }).popup('show');
+                $(this.target).popup({
+                    popup: this.popup,
+                    hoverable: true,
+                    inline: false,
+                    silent: true,
+                    // movePopup: false,
+                    // position: 'top left',
+                    position: 'bottom left',
+                    maxSearchDepth: 50,
+                    delay: {
+                        show: 300,
+                        hide: 300
+                    }
+                }).popup('show');
+            });
         });
     }
 
