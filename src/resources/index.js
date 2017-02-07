250ee289c39e4f61beff2cff00745173385be4b8
weicheng <weicheng.xi@newtouch.cn>
2017-02-07 13:34:12 +0800

    消息菜单折叠整理到dropdown

--- src/resources/index.js
+++ src/resources/index.js
@@ -22,6 +22,7 @@ export function configure(aurelia) {
         'resources/attributes/attr-c2c',
         'resources/attributes/attr-dimmer',
         'resources/attributes/attr-ui-dropdown',
+        'resources/attributes/attr-ui-dropdown-action',
         'resources/attributes/attr-ui-tab',
         'resources/attributes/attr-tablesort',
         'resources/attributes/attr-textcomplete',
