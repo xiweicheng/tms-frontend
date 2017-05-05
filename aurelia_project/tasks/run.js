c50941487a9a9bda42026fb8517f0245b669d79b
weicheng xi <weicheng.xi@newtouch.cn>
2017-05-05 21:06:41 +0800

    blog tag

--- aurelia_project/tasks/run.js
+++ aurelia_project/tasks/run.js
@@ -34,8 +34,9 @@ let serve = gulp.series(
                     },
                     proxyMiddleware(['/free', '/admin', '/upload', '/lib'], {
                         // target: 'http://translation.sh1.newtouch.com',
+                        target: 'http://tms000.sh1.newtouch.com',
                         // target: 'https://xiweicheng.com',
-                        target: 'http://localhost',
+                        // target: 'http://localhost',
                         changeOrigin: true
                     }),
                 ]
