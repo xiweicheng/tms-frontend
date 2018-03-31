9860f2131b809558fd95156dbf4b002d0c37c844
xiweicheng <xiweicheng@yeah.net>
2018-03-31 13:43:10 +0800

    todo

--- src/resources/elements/em-chat-todo.js
+++ src/resources/elements/em-chat-todo.js
@@ -5,6 +5,9 @@ export class EmChatTodo {
 
     @bindable actived;
     loginUser = nsCtx.loginUser;
+    title;
+    todos = [];
+    dones = [];
 
     activedChanged(newValue, oldValue) {
         if (!newValue || this.actived.payload.action != nsCons.ACTION_TYPE_TODO) {
@@ -15,11 +18,77 @@ export class EmChatTodo {
     }
 
     listMy() {
-        $.get('/admin/todo/listMy', {}, (data) => {
+        this.ajax = $.get('/admin/todo/listMy', {}, (data) => {
             if (data.success) {
-                this.todos = data.data;
+                this.todos = _.reject(data.data, { status: 'Done' });
+                this.dones = _.filter(data.data, { status: 'Done' });
             } else {
                 toastr.error(data.data, '获取待办事项列表失败！');
+            }
+        });
+    }
+
+    addTodoHandler() {
+
+    	$(this.todoInputRef).focus();
+    	
+        if (!this.title) {
+            toastr.error('请输入待办事项内容！');
+            return;
+        }
+
+        this.ajax = $.post('/admin/todo/create', { title: this.title }, (data, textStatus, xhr) => {
+            if (data.success) {
+                this.title = '';
+                this.todos = [data.data, ...this.todos];
+            } else {
+                toastr.error(data.data, '创建待办事项失败！');
+            }
+        });
+    }
+
+    statusToggleHandler(item) {
+        $.post('/admin/todo/update', { id: item.id, status: item.status == 'New' ? 'Doing' : "New" }, (data, textStatus, xhr) => {
+            if (data.success) {
+                item.status = data.data.status;
+            } else {
+                toastr.error(data.data, '更新待办事项失败！');
+            }
+        });
+    }
+
+    statusDoneHandler(item) {
+        $.post('/admin/todo/update', { id: item.id, status: 'Done' }, (data, textStatus, xhr) => {
+            if (data.success) {
+                this.todos = _.reject(this.todos, { id: item.id });
+                this.dones = [data.data, ...this.dones];
+            } else {
+                toastr.error(data.data, '更新待办事项失败！');
+            }
+        });
+    }
+
+    statusNewHandler(item) {
+        $.post('/admin/todo/update', { id: item.id, status: 'New' }, (data, textStatus, xhr) => {
+            if (data.success) {
+                this.dones = _.reject(this.dones, { id: item.id });
+                this.todos = [data.data, ...this.todos];
+            } else {
+                toastr.error(data.data, '更新待办事项失败！');
+            }
+        });
+    }
+
+    delHandler(item, type) {
+        $.post(`/admin/todo/delete/${item.id}`, {}, (data, textStatus, xhr) => {
+            if (data.success) {
+                if (type == 'todo') {
+                    this.todos = _.reject(this.todos, { id: item.id });
+                } else {
+                    this.dones = _.reject(this.dones, { id: item.id });
+                }
+            } else {
+                toastr.error(data.data, '删除待办事项失败！');
             }
         });
     }
