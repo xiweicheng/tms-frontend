5060692695a74b5738fbbffda9ef2645a4a03e8e
xiweicheng <xiweicheng@yeah.net>
2018-08-04 13:51:33 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -13,10 +13,11 @@ echo "au pkg --env prod"
 cd $SRC
 au pkg --env prod
 
-echo "rm page/scripts & page/index.html"
+echo "rm page/scripts & page/index.html & page/blog.html"
 
 rm -rf $DEST/page/scripts
 rm -rf $DEST/page/index.html
+rm -rf $DEST/page/blog.html
 rm -rf $DEST/page/alert.mp3
 rm -rf $DEST/page/alert.ogg
 
@@ -24,6 +25,7 @@ echo "cp tms-frontend to tms"
 
 cp -rf $SRC/scripts $DEST/page
 cp -rf $SRC/index.html $DEST/page
+cp -rf $SRC/blog.html $DEST/page
 cp -rf alert.mp3 $DEST/page
 cp -rf alert.ogg $DEST/page
 
