c40cd44ceff9b6e43b42755e2816458bb23c782c
xiweicheng <xiweicheng@yeah.net>
2018-12-01 21:18:37 +0800

    ...

--- src/resources/elements/em-blog-left-sidebar.js
+++ src/resources/elements/em-blog-left-sidebar.js
@@ -66,6 +66,7 @@ export class EmBlogLeftSidebar {
         });
         this.subscribe2 = ea.subscribe(nsCons.EVENT_BLOG_SWITCH, (payload) => {
             this.blog = _.find(this.blogs, { id: +nsCtx.blogId });
+            this.calcTree();
             this.blog && _.delay(() => this._scrollTo(this.blog.id), 1000);
         });
         this.subscribe3 = ea.subscribe(nsCons.EVENT_BLOG_TOGGLE_SIDEBAR, (payload) => {
