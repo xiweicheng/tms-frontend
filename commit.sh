c1f3af06843b5f256e8485912b12b6a7e8ecfb9d
xiweicheng <xiweicheng@yeah.net>
2018-12-09 12:26:24 +0800

    界面优化...

--- commit.sh
+++ commit.sh
@@ -2,20 +2,21 @@
 
 echo "auto commit start"
 
+git add .
+git commit -m "界面优化..."
+
+git push origin master
 git push github master
 
 sh build.sh
 
 cd  ../tms
-
 pwd
 
 git add .
-
 git commit -m "合并前端静态打包资源"
 
 git push origin master
-
 git push github master
 
 echo "auto commit end"
