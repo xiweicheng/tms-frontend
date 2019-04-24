8160cec125cde1d49e0554ef31d2afedb2f1962e
xiweicheng <xiweicheng@yeah.net>
2019-04-24 21:20:16 +0800

    dropzone销毁

--- src/resources/attributes/attr-dropzone.js
+++ src/resources/attributes/attr-dropzone.js
@@ -4,6 +4,9 @@ import {
     EventAggregator
 }
 from 'aurelia-event-aggregator';
+import {
+    default as Dropzone
+} from 'dropzone';
 
 @customAttribute('dropzone')
 @inject(Element, EventAggregator)
@@ -12,6 +15,8 @@ export class AttrDropzone {
     @bindable clickable;
     @bindable target;
     @bindable type;
+
+    dropzones = [];
 
     constructor(element, eventAggregator) {
         this.element = element;
@@ -31,60 +36,81 @@ export class AttrDropzone {
 
         $(this.element).parent().addClass('tms-dropzone-preview-hidden');
 
-        $(this.element).children().andSelf().dropzone({
-            url: "/admin/file/upload",
-            paramName: 'file',
-            clickable: !!this.clickable,
-            dictDefaultMessage: '',
-            maxFilesize: 10,
-            addRemoveLinks: true,
-            // previewsContainer: this.chatStatusBarRef,
-            // previewTemplate: this.previewTemplateRef.innerHTML,
-            dictCancelUpload: '取消上传',
-            dictCancelUploadConfirmation: '确定要取消上传吗?',
-            dictFileTooBig: '文件过大({{filesize}}M),最大限制:{{maxFilesize}}M',
-            init: function() {
-                this.on("sending", function(file, xhr, formData) {
-                    formData.append('toType', toType);
-                    if('Blog' !== toType) {
-                        formData.append('toId', nsCtx.chatTo);
-                    }
-                });
-                this.on("success", function(file, data) {
-                    if (data.success) {
+        let elms = $(this.element).children().andSelf().toArray();
 
-                        $.each(data.data, function(index, item) {
-                            if (item.type == 'Image') {
-                                $(target).insertAtCaret('![{name}]({baseURL}{path}{uuidName}) '
-                                    .replace(/\{name\}/g, item.name)
-                                    .replace(/\{baseURL\}/g, utils.getBaseUrl() + '/')
-                                    .replace(/\{path\}/g, item.path)
-                                    .replace(/\{uuidName\}/g, item.uuidName));
-                            } else {
-                                $(target).insertAtCaret('[{name}]({baseURL}{path}{uuidName}) '
-                                    .replace(/\{name\}/g, item.name)
-                                    .replace(/\{baseURL\}/g, utils.getBaseUrl() + '/')
-                                    .replace(/\{path\}/g, "admin/file/download/")
-                                    .replace(/\{uuidName\}/g, item.id));
-                            }
-                        });
-                        toastr.success('上传成功!');
-                    } else {
-                        toastr.error(data.data, '上传失败!');
-                    }
 
-                });
-                this.on("error", function(file, errorMessage, xhr) {
-                    toastr.error(errorMessage, '上传失败!');
-                });
-                this.on("complete", function(file) {
-                    this.removeFile(file);
-                });
-            }
+        $.each(elms, (i, elm) => {
+
+            let dropzone = new Dropzone(elm, {
+                url: "/admin/file/upload",
+                paramName: 'file',
+                clickable: !!this.clickable,
+                dictDefaultMessage: '',
+                maxFilesize: 10,
+                addRemoveLinks: true,
+                // previewsContainer: this.chatStatusBarRef,
+                // previewTemplate: this.previewTemplateRef.innerHTML,
+                dictCancelUpload: '取消上传',
+                dictCancelUploadConfirmation: '确定要取消上传吗?',
+                dictFileTooBig: '文件过大({{filesize}}M),最大限制:{{maxFilesize}}M',
+                init: function() {
+                    this.on("sending", function(file, xhr, formData) {
+                        formData.append('toType', toType);
+                        if ('Blog' !== toType) {
+                            formData.append('toId', nsCtx.chatTo);
+                        }
+                    });
+                    this.on("success", function(file, data) {
+                        if (data.success) {
+
+                            $.each(data.data, function(index, item) {
+                                if (item.type == 'Image') {
+                                    $(target).insertAtCaret('![{name}]({baseURL}{path}{uuidName}) '
+                                        .replace(/\{name\}/g, item.name)
+                                        .replace(/\{baseURL\}/g, utils.getBaseUrl() + '/')
+                                        .replace(/\{path\}/g, item.path)
+                                        .replace(/\{uuidName\}/g, item.uuidName));
+                                } else {
+                                    $(target).insertAtCaret('[{name}]({baseURL}{path}{uuidName}) '
+                                        .replace(/\{name\}/g, item.name)
+                                        .replace(/\{baseURL\}/g, utils.getBaseUrl() + '/')
+                                        .replace(/\{path\}/g, "admin/file/download/")
+                                        .replace(/\{uuidName\}/g, item.id));
+                                }
+                            });
+                            toastr.success('上传成功!');
+                        } else {
+                            toastr.error(data.data, '上传失败!');
+                        }
+
+                    });
+                    this.on("error", function(file, errorMessage, xhr) {
+                        toastr.error(errorMessage, '上传失败!');
+                    });
+                    this.on("complete", function(file) {
+                        this.removeFile(file);
+                    });
+                }
+            });
+
+            this.dropzones.push(dropzone);
         });
     }
 
     bind(bindingContext) {
         this.valueChanged(this.value);
+    }
+
+    /**
+     * 当数据绑定引擎从视图解除绑定时被调用
+     */
+    unbind() {
+        try {
+            $.each(this.dropzones, (i, dropzone) => {
+                dropzone.destroy();
+            });
+        } catch (e) {
+            console.log(e);
+        }
     }
 }
