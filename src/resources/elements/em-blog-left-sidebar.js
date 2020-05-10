600da858926b20a95db87fb62c646cfb804c7bbb
xiweicheng <xiweicheng@yeah.net>
2020-05-10 12:02:17 +0800

    blog sortable

--- src/resources/elements/em-blog-left-sidebar.js
+++ src/resources/elements/em-blog-left-sidebar.js
@@ -118,6 +118,26 @@ export class EmBlogLeftSidebar {
         // this._refreshSysLinks();
         this._refreshBlogStows();
 
+        _.delay(() => {
+            $('.tms-sortable-elem').each((i, e) => {
+                Sortable.create(e, {
+                    onEnd: function (evt) {
+                        // var itemEl = evt.item; // dragged HTMLElement
+                        // evt.to; // target list
+                        // evt.from; // previous list
+                        // evt.oldIndex; // element's old index within old parent
+                        // evt.newIndex; // element's new index within new parent
+                        // evt.oldDraggableIndex; // element's old index within old parent, only counting draggable elements
+                        // evt.newDraggableIndex; // element's new index within new parent, only counting draggable elements
+                        // evt.clone // the clone element
+                        // evt.pullMode; // when item is in another sortable: `"clone"` if cloning, `true` if moving
+                        console.log(evt);
+                        
+                    },
+                });
+            })
+        }, 2000);
+
     }
 
     _recentOpenSave(blog) {
