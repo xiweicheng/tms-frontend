1405072195baf486b3cbf0de3a975f196710aff7
xiweicheng <xiweicheng@yeah.net>
2021-09-20 20:21:12 +0800

    界面优化...

--- deploy2local-build-no.sh
+++ deploy2local-build-no.sh
@@ -8,21 +8,19 @@ SRC=/Users/xiweicheng/tms-frontend
 echo "DEST DIR: $DEST"
 echo "SRC DIR: $SRC"
 
-echo "au pkg --env prod"
+# echo "au pkg --env prod"
 
 cd $SRC
-au pkg --env prod
+# au pkg --env prod
 
 DT=`date +"%Y%m%d%H%M%S"`
 mkdir -p $DEST/backup/tms-frontend/$DT
 cp -rf $DEST/webapps/ROOT/WEB-INF/classes/static/page $DEST/backup/tms-frontend/$DT
 echo "backup path: $DEST/backup/tms/tms-frontend/$DT"
 
-echo "rm cdn & scripts & index.html & blog.html & mind.html & excel.html"
+echo "rm cdn & scripts & index.html & blog.html & mind.html & excel.html & sheet.html"
 
 rm -rf $DEST/webapps/ROOT/WEB-INF/classes/static/page/cdn
-rm -rf $DEST/webapps/ROOT/WEB-INF/classes/static/page/scripts
-rm -rf $DEST/webapps/ROOT/WEB-INF/classes/static/page/index.html
 rm -rf $DEST/webapps/ROOT/WEB-INF/classes/static/page/blog.html
 rm -rf $DEST/webapps/ROOT/WEB-INF/classes/static/page/mind.html
 rm -rf $DEST/webapps/ROOT/WEB-INF/classes/static/page/excel.html
@@ -31,8 +29,6 @@ rm -rf $DEST/webapps/ROOT/WEB-INF/classes/static/page/sheet.html
 echo "cp tms-frontend to local tomcat"
 
 cp -rf $SRC/cdn $DEST/webapps/ROOT/WEB-INF/classes/static/page
-cp -rf $SRC/scripts $DEST/webapps/ROOT/WEB-INF/classes/static/page
-cp -rf $SRC/index.html $DEST/webapps/ROOT/WEB-INF/classes/static/page
 cp -rf $SRC/blog.html $DEST/webapps/ROOT/WEB-INF/classes/static/page
 cp -rf $SRC/mind.html $DEST/webapps/ROOT/WEB-INF/classes/static/page
 cp -rf $SRC/excel.html $DEST/webapps/ROOT/WEB-INF/classes/static/page
