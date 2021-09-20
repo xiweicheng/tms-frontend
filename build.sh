43544a84b7fa66af7643edcf88b946ccedcab97d
xiweicheng <xiweicheng@yeah.net>
2021-09-20 10:40:26 +0800

    luckysheet表格

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ echo "build start"
 DEST=/Users/xiweicheng/tms/src/main/resources/static
 SRC=/Users/xiweicheng/tms-frontend
 
+DEST2=/Users/xiweicheng/tms-landing
+
 echo "DEST DIR: $DEST"
 echo "SRC DIR: $SRC"
 
@@ -40,5 +42,17 @@ cp -rf $SRC/cdn $DEST/page
 # cp -rf alert.ogg $DEST/page
 # cp -rf $SRC/font-awesome.min.css $DEST/page
 # cp -rf $SRC/fonts $DEST/page
+
+rm -rf $DEST2/page/blog.html
+rm -rf $DEST2/page/excel.html
+rm -rf $DEST2/page/sheet.html
+rm -rf $DEST2/page/mind.html
+rm -rf $DEST2/page/cdn
+
+cp -rf $SRC/blog.html $DEST2/page
+cp -rf $SRC/excel.html $DEST2/page
+cp -rf $SRC/sheet.html $DEST2/page
+cp -rf $SRC/mind.html $DEST2/page
+cp -rf $SRC/cdn $DEST2/page
 
 echo "build end"
